8f4f813f64554ddf94b9d2bc81ee6098ecdc70c2
Moryanmeena <98578202+Moryanmeena@users.noreply.github.com>
2025-02-28 18:41:58 +0530

    Delete api/admin/publicWallet.php

--- api/admin/publicWallet.php
+++
@@ -1,7 +0,0 @@
-<?phpheader("Content-Type: application/json");
-
-// Hardcoded Wallet Address (Replace with Your Own)
-$walletAddress = "0xd6B56E53b3C5e69C05ca5197D3DF4cB310a30fba"; 
-
-echo json_encode(["walletAddress" => $walletAddress]);
-?>
